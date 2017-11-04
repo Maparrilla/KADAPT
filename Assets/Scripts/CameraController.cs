0327c34f42a319fe004875c5b499fd930924f22c
Maparrilla <mparrilla96@gmail.com>
2017-11-04 18:24:12 -0400

    Add files via upload

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+    public float sensitivityX = 5F;
+    public float sensitivityY = 5F;
+
+    public float minimumX = -360F;
+    public float maximumX = 360F;
+
+    public float minimumY = -60F;
+    public float maximumY = 60F;
+
+    float rotationY = 0F;
+
+    public enum KEYBOARD_INPUT : int
+    {
+        RIGHT = KeyCode.D,
+        LEFT = KeyCode.A,
+        FORWARD = KeyCode.W,
+        BACK = KeyCode.S,
+        UP = KeyCode.E,
+        DOWN = KeyCode.Q
+    }
+
+    public int moveSpeed;
+    public int scrollSpeed;
+
+    public float sensitivity;
+
+    private Camera myCam;
+    private Transform g_Camera;
+
+    // Use this for initialization
+    void Start()
+    {
+        moveSpeed = 6;
+        scrollSpeed = 10;
+
+        sensitivity = 0.02f;
+
+        myCam = GetComponent<Camera>();
+        g_Camera = myCam.transform;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        /*Vector3 vp = myCam.ScreenToViewportPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, myCam.nearClipPlane));
+        vp.x -= 0.5f;
+        vp.y -= 0.5f;
+        vp.x *= sensitivity;
+        vp.y *= sensitivity;
+        vp.x += 0.5f;
+        vp.y += 0.5f;
+        Vector3 sp = myCam.ViewportToScreenPoint(vp);
+
+        Vector3 v = myCam.ScreenToWorldPoint(sp);
+        transform.LookAt(v, Vector3.up);*/
+
+
+        if (axes == RotationAxes.MouseXAndY)
+        {
+            float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+            transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+        }
+        else if (axes == RotationAxes.MouseX)
+        {
+            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+        }
+        else
+        {
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+            transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+            Cursor.lockState = CursorLockMode.None;
+        else
+            Cursor.lockState = CursorLockMode.Locked;
+
+        foreach (KEYBOARD_INPUT val in System.Enum.GetValues(typeof(KEYBOARD_INPUT)))
+        {
+            if (Input.GetKey((KeyCode)val))
+            {
+                switch (val)
+                {
+                    case KEYBOARD_INPUT.FORWARD:
+                        g_Camera.Translate(0, 0, 1 * moveSpeed * Time.deltaTime, Space.Self);
+                        break;
+                    case KEYBOARD_INPUT.BACK:
+                        g_Camera.Translate(0, 0, -1 * moveSpeed * Time.deltaTime, Space.Self);
+                        break;
+                    case KEYBOARD_INPUT.LEFT:
+                        g_Camera.Translate(-1 * moveSpeed * Time.deltaTime, 0, 0, Space.Self);
+                        break;
+                    case KEYBOARD_INPUT.RIGHT:
+                        g_Camera.Translate(1 * moveSpeed * Time.deltaTime, 0, 0, Space.Self);
+                        break;
+                    case KEYBOARD_INPUT.UP:
+                        g_Camera.Translate(0, 1 * moveSpeed * Time.deltaTime, 0, Space.Self);
+                        break;
+                    case KEYBOARD_INPUT.DOWN:
+                        g_Camera.Translate(0, -1 * moveSpeed * Time.deltaTime, 0, Space.Self);
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/InvokeEventB3.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TreeSharpPlus;
+
+public class InvokeEventB3 : MonoBehaviour {
+
+    public GameObject Daniel;
+    public GameObject Richard;
+    public GameObject Ethan;
+
+    public Vector3 offset = new Vector3(0.1f, 0, -0.1f);
+
+    private BehaviorAgent behaviorAgent;
+
+    protected Node DanRichGreeting()
+    {
+        return new Sequence(
+            Daniel.GetComponent<BehaviorMecanim>().ST_PlayHandGesture("Wave", 3500),
+            Richard.GetComponent<BehaviorMecanim>().ST_PlayHandGesture("Wave", 3500),
+            Daniel.GetComponent<BehaviorMecanim>().ST_PlayBodyGesture("being_cocky", 1633),
+            Richard.GetComponent<BehaviorMecanim>().ST_PlayBodyGesture("look_away", 2000),
+            Richard.GetComponent<BehaviorMecanim>().ST_PlayHandGesture("Wave", 3500),
+            Daniel.GetComponent<BehaviorMecanim>().ST_PlayHandGesture("Wave", 3500));
+    }
+
+    protected Node RichEthanGreeting()
+    {
+        return new Sequence(
+            Richard.GetComponent<BehaviorMecanim>().ST_PlayHandGesture("Wave", 3500),
+            Ethan.GetComponent<BehaviorMecanim>().ST_PlayHandGesture("Wave", 3500),
+            Richard.GetComponent<BehaviorMecanim>().ST_PlayBodyGesture("being_cocky", 1633),
+            Ethan.GetComponent<BehaviorMecanim>().ST_PlayBodyGesture("look_away", 2000),
+            Ethan.GetComponent<BehaviorMecanim>().ST_PlayHandGesture("Wave", 3500),
+            Richard.GetComponent<BehaviorMecanim>().ST_PlayHandGesture("Wave", 3500));
+    }
+
+    protected Node DanEthanGreeting()
+    {
+        return new Sequence(
+            Ethan.GetComponent<BehaviorMecanim>().ST_PlayHandGesture("Wave", 3500),
+            Daniel.GetComponent<BehaviorMecanim>().ST_PlayHandGesture("Wave", 3500),
+            Ethan.GetComponent<BehaviorMecanim>().ST_PlayBodyGesture("being_cocky", 1633),
+            Daniel.GetComponent<BehaviorMecanim>().ST_PlayBodyGesture("look_away", 2000),
+            Daniel.GetComponent<BehaviorMecanim>().ST_PlayHandGesture("Wave", 3500),
+            Ethan.GetComponent<BehaviorMecanim>().ST_PlayHandGesture("Wave", 3500));
+    }
+
+    protected Node ApproachAndOrientDR(Val<Vector3> DanielPos, Val<Vector3> RichardPos)
+    {
+        return new Sequence(
+            Daniel.GetComponent<BehaviorMecanim>().Node_GoTo(RichardPos),
+            new SequenceParallel(
+                Daniel.GetComponent<BehaviorMecanim>().Node_OrientTowards(RichardPos),
+                Richard.GetComponent<BehaviorMecanim>().Node_OrientTowards(DanielPos)));
+    }
+
+    protected Node ApproachAndOrientRE(Val<Vector3> RichardPos, Val<Vector3> EthanPos)
+    {
+        return new SequenceParallel(
+            Richard.GetComponent<BehaviorMecanim>().Node_GoTo(EthanPos),
+            new SequenceParallel(
+                Ethan.GetComponent<BehaviorMecanim>().Node_OrientTowards(RichardPos),
+                Richard.GetComponent<BehaviorMecanim>().Node_OrientTowards(EthanPos)));
+    }
+
+    protected Node ApproachAndOrientDE(Val<Vector3> DanielPos, Val<Vector3> EthanPos)
+    {
+        return new SequenceParallel(
+            Ethan.GetComponent<BehaviorMecanim>().Node_GoTo(DanielPos),
+            new SequenceParallel(
+                Daniel.GetComponent<BehaviorMecanim>().Node_OrientTowards(EthanPos),
+                Ethan.GetComponent<BehaviorMecanim>().Node_OrientTowards(DanielPos)));
+    }
+
+    protected Node GreetingTree()
+    {
+        Val<Vector3> DanielPos = Val.V(() => Daniel.transform.position + offset);
+        Val<Vector3> RichardPos = Val.V(() => Richard.transform.position + offset);
+        Val<Vector3> EthanPos = Val.V(() => Ethan.transform.position + offset);
+
+        return new SequenceShuffle(
+            new Sequence(
+                this.ApproachAndOrientDR(DanielPos, RichardPos),
+                this.DanRichGreeting()),
+            new Sequence(
+                this.ApproachAndOrientRE(RichardPos, EthanPos),
+                this.RichEthanGreeting()),
+            new Sequence(
+                this.ApproachAndOrientDE(DanielPos, EthanPos),
+                this.DanEthanGreeting()));
+    }
+
+    // Use this for initialization
+    void Start () {
+        behaviorAgent = new BehaviorAgent(this.BuildTreeRoot());
+        BehaviorManager.Instance.Register(behaviorAgent);
+        behaviorAgent.StartBehavior();
+    }
+
+    protected Node BuildTreeRoot()
+    {
+        return new Sequence(
+            this.GreetingTree());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
